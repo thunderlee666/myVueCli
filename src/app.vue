4530694c2d3063032f9c03a280fb9e6b88a09bc7
李雷杰 <lileijie@lileijiedeMacBook-Pro.local>
2020-09-25 10:12:58 +0800

    初始化

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{this.msg}}</div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from "vue-property-decorator";
+
+  @Component
+  export default class App extends Vue {
+    msg="hello world"
+  }
+</script>
+
+<style scoped>
+
+</style>
